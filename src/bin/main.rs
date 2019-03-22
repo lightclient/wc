7c1e5e008e7d90e604b911f9d4993712749c36d1
Matt Garnett <matt@mattgarnett.com>
2019-03-21 20:15:12 -0500

    moved printing logic to main and added tests

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,6 +1,7 @@
 extern crate clap;
 use clap::{App, Arg};
-use wc::analyze_file;
+use wc::{Result, analyze_file};
+use std::process;
 
 fn main() {
    let matches = App::new("wc")
@@ -8,12 +9,51 @@ fn main() {
              .help("the name of the input file")
              .index(1)
        )
+       .arg(Arg::with_name("print_lines").short("l"))
+       .arg(Arg::with_name("print_words").short("w"))
+       .arg(Arg::with_name("print_bytes").short("c"))
+       .arg(Arg::with_name("print_chars").short("m"))
        .get_matches();
 
 
+    let result: Result;
     if let Some(filename) = matches.value_of("filename") {
-        analyze_file(filename); 
+        result = analyze_file(filename); 
     } else {
         println!("STDIN not implemented yet");
+        process::exit(1);
+    }
+
+    write_counts(result, matches); 
+}
+
+fn write_counts(result: Result, matches: clap::ArgMatches) {
+    let no_args = !(
+        matches.is_present("print_lines") ||
+        matches.is_present("print_words") ||
+        matches.is_present("print_bytes")
+    );
+
+
+    if no_args || matches.is_present("print_lines") {
+        print!("{:8}", result.lines);
+    }
+
+    if no_args || matches.is_present("print_words") {
+        print!("{:8}", result.words);
+    }
+
+    /*
+    if matches.is_present("print_chars") {
+        print!("{:8}", result.chars);
+    }
+    */
+
+    if no_args || matches.is_present("print_bytes") {
+        print!("{:8}", result.bytes);
+    }
+
+    if let Some(filename) = matches.value_of("filename") {
+        print!(" {:8}", filename);
     }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,18 @@
 use std::fs;
 use std::process;
 
-struct Results {
-    bytes: u32,
-    lines: u32,
-    words: u32,
+#[derive(Debug)]
+#[derive(PartialEq)]
+pub struct Result {
+    pub bytes: u32,
+    pub lines: u32,
+    pub words: u32,
     in_word: bool,
 }
 
-impl Results {
-    fn new() -> Results {
-       Results {
+impl Result {
+    fn new() -> Result {
+       Result {
            bytes: 0,
            lines: 0,
            words: 0,
@@ -19,20 +21,24 @@ impl Results {
     }
 }
 
-pub fn analyze_file(filename: &str) {
-    let data = fs::read_to_string(filename).unwrap_or_else(|err| {
+pub fn analyze_file(filename: &str) -> Result {
+    let data = fs::read(filename).unwrap_or_else(|err| {
             eprintln!("Unable to read file: {}", err);
             process::exit(1);
     });
 
-    let results = data.chars().fold(Results::new(), |mut acc, c| {
+    analyze_bytes(data)
+}
+
+pub fn analyze_bytes(data: Vec<u8>) -> Result {
+    let mut result = data.iter().fold(Result::new(), |mut acc, c| {
         acc.bytes += 1;
 
-        if c == '\n' {
+        if *c == '\n' as u8 {
             acc.lines += 1;
             if acc.in_word { acc.words += 1 }
             acc.in_word = false;
-        } else if c.is_whitespace() && acc.in_word {
+        } else if (*c as char).is_whitespace() && acc.in_word {
             acc.words += 1;
             acc.in_word = false;
         } else if !acc.in_word {
@@ -42,5 +48,44 @@ pub fn analyze_file(filename: &str) {
         acc
     });
 
-    println!("{:8}{:8}{:8} {:8}", results.lines, results.words, results.bytes, filename);
+    if result.in_word {
+       result.words += 1;
+       result.in_word = false;
+    }
+
+    result
+}
+
+#[cfg(test)] 
+mod tests {
+    use super::*; 
+
+    #[test]
+    fn test_empty_string() {
+        let test_bytes = "".as_bytes().to_vec();
+        let result = Result { bytes: 0, lines: 0, words: 0, in_word: false };
+        assert_eq!(analyze_bytes(test_bytes), result);
+    }
+
+    #[test]
+    fn test_single_line() {
+        let test_bytes = "Hello world, this is a string!".as_bytes().to_vec();
+        let result = Result { bytes: 30, lines: 0, words: 6, in_word: false };
+        assert_eq!(analyze_bytes(test_bytes), result);
+    }
+
+    #[test]
+    fn test_emojis() {
+        // this emoji should be 4 bytes
+        let test_bytes = "rust is 🔥".as_bytes().to_vec();
+        let result = Result { bytes: 12, lines: 0, words: 3, in_word: false };
+        assert_eq!(analyze_bytes(test_bytes), result);
+    }
+
+    #[test]
+    fn test_newlines() {
+        let test_bytes = "a\n\nb\nc\n\n\nd".as_bytes().to_vec();
+        let result = Result { bytes: 10, lines: 6, words: 4, in_word: false };
+        assert_eq!(analyze_bytes(test_bytes) , result);
+    }
 }
